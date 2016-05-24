3e7dd13e8c095e33af3a1e508d2656f9e03b2756
Forest Hoffman <foresthoffman@users.noreply.github.com>
2016-05-24 00:32:50 -0700

    Updated to 1.1.0 and added GNUPL v2

--- src/FHCounterClass.js
+++ src/FHCounterClass.js
@@ -1,7 +1,22 @@
 /*
  * FHCounterClass by Forest Hoffman, 2016
  *
- * Version: 1.0.9
+ * Version: 1.1.0
+ *
+ * FHCounterClass is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
+ *  
+ * FHCounterClass is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *  
+ * You should have received a copy of the GNU General Public License
+ * along with FHCounterClass. If not, see http://www.gnu.org/licenses/gpl-2.0.txt.
+ *
+ *** *** *** *** *** *** *** *** *** *** *** ***
  *
  * Renders arcs using the 2D canvas renderer in most browsers.
  * 
